587147f5b3c88d40cf62620386bac9c855320186
lemonade-ryan <fundiryan850@gmail.com>
2025-05-19 07:30:26 -0700

    first commit

---
+++ intro.py
@@ -0,0 +1,5 @@
+print("Ryan Fundi")
+
+my_name="ryan fundi"
+
+print(my_name)
---
+++ string-methods.py
@@ -0,0 +1,11 @@
+text=" learn python"
+text_stip=text.strip()
+text_stip=text_stip.title()
+print(text_stip)
+
+
+tet="hello"
+reversed_hello=tet[::-1]
+print(reversed_hello)
+
+re
